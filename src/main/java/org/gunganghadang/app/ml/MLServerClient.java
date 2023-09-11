e6f47d216d0c50ea56430bcae10d3e7e2bea0e3e
genieu99 <yujin0419@sookmyung.ac.kr>
2023-09-11 22:02:15 +0900

    [Fix] 잘못된 요청 수정(#12)

--- src/main/java/org/gunganghadang/app/ml/MLServerClient.java
+++ src/main/java/org/gunganghadang/app/ml/MLServerClient.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.io.InputStream;
